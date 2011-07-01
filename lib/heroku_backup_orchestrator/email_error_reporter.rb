4adbbd8549495a1393077bea56ae90d81c1566da
Peter Skeide <peterskeide@gmail.com>
2011-07-01 20:35:11 +0200

    Configure global Pony (email) options only once

--- lib/heroku_backup_orchestrator/email_error_reporter.rb
+++ lib/heroku_backup_orchestrator/email_error_reporter.rb
@@ -1,21 +1,18 @@
 module HerokuBackupOrchestrator
-  class EmailErrorReporter
-    def initialize
-      smtp_options = {
+  class EmailErrorReporter   
+    
+    Pony.options = {
+      :from => CONFIG['error_email'], :to => CONFIG['error_email'],
+      :subject => 'BACKUP ERROR', :via => :smtp, :via_options => {
         :address        => 'smtp.sendgrid.net',
         :port           => '25',
         :authentication => :plain,
         :user_name      => ENV['SENDGRID_USERNAME'],
         :password       => ENV['SENDGRID_PASSWORD'],
         :domain         => ENV['SENDGRID_DOMAIN']
-      }
-      Pony.options = {
-        :from => CONFIG['error_email'], :to => CONFIG['error_email'],
-        :via => :smtp, :via_options => smtp_options,
-        :subject => 'BACKUP ERROR'
-      }
-    end
-    
+      }    
+    }
+
     def report(error)
       body = %{
         An error occurred during backup: #{error.inspect}
@@ -26,5 +23,6 @@ module HerokuBackupOrchestrator
       }
       Pony.mail(:body => body)
     end
+
   end 
 end
