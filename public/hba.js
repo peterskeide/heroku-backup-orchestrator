b8ea3c5ee7ea4ea8b7acbe93c9c0e0b98069e7cb
Peter Skeide <peterskeide@gmail.com>
2011-12-20 07:39:21 +0100

    Refactor javascript
     - Remove implicit global
     - Move var declarations to top of function

--- public/hba.js
+++ public/hba.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
 	
 	$('#manual_backup_button').click(function() {
+		var callback, applicationName;
+		
 		$(this).attr('disabled', 'disabled');
 		$('#ajax_loader').show();
 		$('#backup_result').html('');
 
 		callback = function(data) {
 			$('#ajax_loader').hide();
-			if ('success' == data.status) {
+			if ('success' === data.status) {
 				$('#backup_result').html('Success! Refresh the page to see the new bundle.').removeClass('error').addClass('success');
 			} else {
 				$('#backup_result').html('Error! ' + data.message).removeClass('success').addClass('error');
@@ -15,8 +17,8 @@ $(document).ready(function() {
 			$('#manual_backup_button').removeAttr("disabled");
 		};
 	
-		var application_name = $(this).attr('data-application-name');
-		$.post('/applications/' + application_name, callback, 'json');
+		applicationName = $(this).attr('data-application-name');
+		$.post('/applications/' + applicationName, callback, 'json');
 	});
 	
 });
